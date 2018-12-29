a89466bce28a84b6321ad89cce8a5e02f24275b6
Denis Dovzhenko <dendvz@gmail.com>
2018-12-30 01:20:01 +0300

    Labyrinth parser from block comment + code restructuring

--- lua_prototype/Rules_1.lua
+++ lua_prototype/Rules_1.lua
@@ -1,13 +1,3 @@
-Utils = {
-  toString = function (obj)
-    local result = {}
-    for k, v in pairs(obj) do
-      table.insert(result, string.format('%s=%s', k, type(v) == 'table' and Utils.toString(v) or tostring(v)))
-    end
-    return '{ ' .. table.concat(result, ', ') .. ' }'
-  end
-}
-
 Player = setmetatable(
 {
   -- prototype
@@ -16,41 +6,42 @@ Player = setmetatable(
     name   = '',
     x      = 0,
     y      = 0,
-    ammo   = {},
+    ammo   = { bombs = 0, rounds = 0 },
     -- actions
     move = function(self, direction)
       local d = string.lower(direction)
       local msg = 'OK'
       local action = {
-        n = function(p) if Map(p.x, p.y).n == ' ' then p.y = p.y - 1 else error() end end,
-        s = function(p) if Map(p.x, p.y).s == ' ' then p.y = p.y + 1 else error() end end,
-        w = function(p) if Map(p.x, p.y).w == ' ' then p.x = p.x - 1 else error() end end,
-        e = function(p) if Map(p.x, p.y).e == ' ' then p.x = p.x + 1 else error() end end
+        n = function(p) if self.map(p.x, p.y).n == ' ' then p.y = p.y - 1 else error() end end,
+        s = function(p) if self.map(p.x, p.y).s == ' ' then p.y = p.y + 1 else error() end end,
+        w = function(p) if self.map(p.x, p.y).w == ' ' then p.x = p.x - 1 else error() end end,
+        e = function(p) if self.map(p.x, p.y).e == ' ' then p.x = p.x + 1 else error() end end
       }
       local result = pcall(action[d], self)
       if result then
-        local cell = Map(self.x, self.y)._
-        if cell ~= '' and Map[cell] then
-          Map[cell](self)
-          msg = msg .. ' ' .. Map[cell].name
+        local cell = self.map(self.x, self.y)._
+        if cell ~= '' and self.map[cell] then
+          self.map[cell](self)
+          msg = msg .. ' ' .. self.map[cell].name
         end
       else
-        msg = 'WALL(' .. Map(self.x, self.y)[d] .. ')'
+        msg = 'WALL(' .. self.map(self.x, self.y)[d] .. ')'
       end
       print(self.name .. ' move ' .. string.upper(direction) .. ' : ' .. msg)
       return result
     end,
+
     use  = function(self, what, where)
       if what == 'bomb' then
         local result = false
         local msg = 'OK'
         local action = {
-          n = function(p) if Map(p.x, p.y).n == '-' then Map(p.x, p.y).n = ' ' end end,
-          s = function(p) if Map(p.x, p.y).s == '-' then Map(p.x, p.y).s = ' ' end end,
-          w = function(p) if Map(p.x, p.y).w == '|' then Map(p.x, p.y).w = ' ' end end,
-          e = function(p) if Map(p.x, p.y).e == '|' then Map(p.x, p.y).e = ' ' end end
+          n = function(p) if self.map(p.x, p.y).n == '-' then self.map(p.x, p.y).n = ' ' end end,
+          s = function(p) if self.map(p.x, p.y).s == '-' then self.map(p.x, p.y).s = ' ' end end,
+          w = function(p) if self.map(p.x, p.y).w == '|' then self.map(p.x, p.y).w = ' ' end end,
+          e = function(p) if self.map(p.x, p.y).e == '|' then self.map(p.x, p.y).e = ' ' end end
         }
-        if self.ammo.bombs > 0 then
+        if self.ammo and self.ammo.bombs > 0 then
           result = pcall(action[string.lower(where)], self)
           self.ammo.bombs = self.ammo.bombs - 1
         else
@@ -64,7 +55,11 @@ Player = setmetatable(
   -- metatable for Player instance
   instance_mt = {
     __tostring = function (self)
-      return Utils.toString(self)
+      local result = {}
+      for _, k in ipairs({ 'name', 'x', 'y', 'ammo' }) do
+        table.insert(result, string.format('%s=%s', k, self.map:toString(self[k])))
+      end
+      return '{ ' .. table.concat(result, ', ') .. ' }'
     end,
     __index = function (self, key)
       return Player.proto[key]
@@ -87,70 +82,6 @@ Player = setmetatable(
   end
 })
 
-Map = setmetatable(
-{
-  SizeX   = 0,
-  SizeY   = 0,
-  Objects = {},
-  Cells   = {},
-
-  -- metatable for cell
-  cell_mt = {
-    __index = function (self, key)
-      assert(string.len(key) == 1 and string.find('news_', key), 'invalid index \'' .. key .. '\' requested')
-      if key == 'n' then return Map.Cells[self.x][self.y - 1].s end
-      if key == 'w' then return Map.Cells[self.x - 1][self.y].e end
-      return Map.Cells[self.x][self.y][key]
-    end,
-    __newindex = function (self, key, value)
-      assert(string.len(key) == 1 and string.find('news_', key), 'invalid index \'' .. key .. '\' requested')
-      if key == 'n' then
-        Map.Cells[self.x][self.y - 1].s = value
-      elseif key == 'w' then
-        Map.Cells[self.x - 1][self.y].e = value
-      else
-        Map.Cells[self.x][self.y][key] = value
-      end
-      return Map(self.x, self.y)
-    end,
-    __tostring = function (self)
-      local n = string.rep(Map.Cells[self.x][self.y - 1].s, 3)
-      local w = Map.Cells[self.x - 1][self.y].e
-      local e = Map.Cells[self.x][self.y].e
-      local s = string.rep(Map.Cells[self.x][self.y].s, 3)
-      local loc = Map.Cells[self.x][self.y]['_']
-      if string.len(loc) == 1 then loc = loc .. ' ' end
-      return string.format('+%s+\n%s%3s%s\n+%s+', n, w, loc, e, s)
-    end
-  },
-
-  init = function(self, sizeX, sizeY)
-    self.SizeX = sizeX
-    self.SizeY = sizeY
-
-    for x = 0, sizeX do
-      self.Cells[x] = {}
-      for y = 0, sizeY do
-        -- set up walls
-        self.Cells[x][y] = {
-          x = x,
-          y = y,
-          e = ((x == 0 or x == SizeX) and '#' or ' '),
-          s = ((y == 0 or y == SizeY) and '#' or ' ')
-        }
-        self.Cells[x][y]['_'] = ''
-      end
-    end
-  end
-},
-{
-  __call = function(self, x, y)
-    assert( 1 <= x and x <= self.SizeX and 1 <= y and y <= self.SizeY,
-            string.format('cell Map(%d,%d) is out of range', x , y))
-    return setmetatable({ x = x, y = y }, Map.cell_mt)
-  end
-})
-
 Object = setmetatable(
 {},
 {
@@ -158,40 +89,3 @@ Object = setmetatable(
     return setmetatable({ name = name }, { __call = func })
   end
 })
-
--- DEMO
---
---
---
--- initialize map 10x10
-Map:init(10, 10)
--- build wall
-Map(5, 2).s = '-'
--- set up Arsenal
-Map(5, 3)._ = 'A'
-
-Map.A = Object('Arsenal', function (self, player)
-  player.ammo = {
-    bombs = 3,
-    rounds = 3
-  } 
-end)
-
--- dummy bot: walk north, demolish walls until running out of bombs
-function dummy(player)
-  repeat
-    if not player:move('N') then
-      player:use('bomb', 'N')
-    end
-  until player.ammo.bombs == 0
-end
-
-p1 = Player { name = 'Alice', x = 5, y = 5 }
-print(p1)
-
--- initialize with defaults
-p2 = Player 'Bob'
-print(p2)
-
--- Run bot
-dummy(p1)
